9c6515fcd12341f457b36f1f7c021f83952f0818
Ilya Zakharov <ilushamdmaa@yandex.ru>
2023-02-17 01:19:44 +0300

    add conftest.py and test_items.py

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome import options
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='ru',
+                     help="Choose language: '--language = lang ' ")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options_ = Options()
+    options_.add_experimental_option(
+        'prefs', {'intl.accept_languages': user_language})
+    print("\nstart chrome browser for test..")
+    browser = webdriver.Chrome(options=options_)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
---
+++ test_items.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+
+def test_find_button_add_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    browser.implicitly_wait(5)
+    browser.get(link)
+    try:
+        browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+        flag_but = True
+
+    except:
+
+        flag_but = False
+
+    assert flag_but, 'Такой кнопки нет ;('
